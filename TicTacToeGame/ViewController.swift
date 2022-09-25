b5db8ebcaf61363415a0e77e354cc236724aece0
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-25 14:46:46 +0300

    Initial Commit

---
+++ TicTacToeGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToeGame
+//
+//  Created by Алексей Павленко on 25.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
+
