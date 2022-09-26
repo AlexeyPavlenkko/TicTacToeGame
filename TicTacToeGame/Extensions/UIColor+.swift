23585101367f248fb809d051fc54920dc0c28438
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-26 16:01:14 +0300

    extension UIColor to get apps colors faster and easier

---
+++ TicTacToeGame/Extensions/UIColor+.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+.swift
+//  TicTacToeGame
+//
+//  Created by Алексей Павленко on 25.09.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let softWhite = UIColor(named: "softWhite")!
+    static let main = UIColor(named: "main")!
+    static let secondary = UIColor(named: "secondary")!
+    static let modeGreen = UIColor(named: "green")!
+    static let modeYellow = UIColor(named: "yellow")!
+    static let modeRed = UIColor(named: "red")!
+}
